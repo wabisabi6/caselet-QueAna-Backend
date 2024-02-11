cd047f63483316acbe873c6d315c1ee3aacf6b4e
Megha Singh <116123164+wabisabi6@users.noreply.github.com>
2024-02-11 15:42:00 -0500

    updating options for create exam request

--- controllers/Exam.js
+++ controllers/Exam.js
@@ -77,7 +77,16 @@ exports.createExam = async (req, res, next) => {
   console.log(req.body);
 
   if (req.method == "OPTIONS") {
-    return  res.status(200);
+    const response = {
+      statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      },
+      body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
   }
 
   let body = req.body;
