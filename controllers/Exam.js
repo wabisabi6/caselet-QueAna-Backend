c19f91ef3f33c5e16d8de5b7211b9654f446de48
Megha Singh <116123164+wabisabi6@users.noreply.github.com>
2024-02-11 15:58:31 -0500

    added response headers

--- controllers/Exam.js
+++ controllers/Exam.js
@@ -80,6 +80,7 @@ exports.createExam = async (req, res, next) => {
     const response = {
       statusCode: 200,
       headers: {
+        
           "Access-Control-Allow-Headers" : "Content-Type",
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
