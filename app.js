7c9928e1d531598968876ad38683ecfd750348ee
Megha Singh <116123164+wabisabi6@users.noreply.github.com>
2024-02-11 15:25:19 -0500

    removing some cors headers

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ connection = connectDB();
 // Configure CORS
 const corsOptions = {
   origin: 'https://main-deployment-aws-test-admin.d3brix7vti8n8n.amplifyapp.com',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, // Considering credentials are important for your requests
-  allowedHeaders: ['Content-Type', 'Authorization'], // Ensure 'Authorization' is allowed
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
