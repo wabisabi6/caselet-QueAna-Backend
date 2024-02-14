b247630c4d135add085d27d72ecaefb1a93c921e
Megha Singh <116123164+wabisabi6@users.noreply.github.com>
2024-02-13 21:58:00 -0500

    removing unsupported and deprecated options from connection

--- utils/db.js
+++ utils/db.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   console.log(`MongoDB conn URI: ${process.env.MONGO_URI}`.cyan.underline.bold);
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
   return conn
